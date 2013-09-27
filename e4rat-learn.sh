46758a1c3a5d0182486e55f0fbb9d6596ec86d04
Alan Jenkins <alan.james.jenkins@gmail.com>
2013-09-27 19:10:13 +0100

    Added e4rat-learn and e4rat-run.

--- e4rat-learn.sh
+++ e4rat-learn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- efibootmgr -c -d /dev/sda -p 3 -l /EFI/arch/vmlinuz-arch.efi -L "Arch Linux (EFISTUB)" -u "initrd=/EFI/arch/initramfs-arch.img root=/dev/sdc3 init=/sbin/e4rat-collect"
+ efibootmgr -c -d /dev/sda -p 3 -l /EFI/arch/vmlinuz-arch.efi -L "Arch Linux - E4RAT Learn (EFISTUB)" -u "initrd=/EFI/arch/initramfs-arch.img root=/dev/sdc3 init=/sbin/e4rat-collect"
---
+++ e4rat-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ efibootmgr -c -d /dev/sda -p 3 -l /EFI/arch/vmlinuz-arch.efi -L "Arch Linux - E4RAT (EFISTUB)" -u "initrd=/EFI/arch/initramfs-arch.img root=/dev/sdc3 init=/sbin/e4rat-preload"
